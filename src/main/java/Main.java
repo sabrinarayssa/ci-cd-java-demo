fd3f10f228f81d1717961c478f7bcb95577e2c69
Gustavo Fernandes <gustavo.fernandes-ext2@ab-inbev.com>
2024-11-20 18:34:21 -0300

    refactoring method/test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, CI/CD with GitHub Actions!");
+
+        System.out.println("Sorted Array: " + Arrays.toString(sortArray(new int[]{5, 3, 1, 8, 1, 2})));
     }
 
-    public int add(int a, int b) {
-        return a + b;
+    public static int[] sortArray(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,19 +1,12 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MainTest {
 
-    private Main main;
-
-    @BeforeEach
-    void setUp() {
-        main = new Main();
-    }
-
     @Test
-    public void testAdd() {
-        assertEquals(5, main.add(2, 3));
+    public void testSortArray() {
+        int[] input = {13, 5, 3, 1, 8, 1, 2};
+        int[] expected = {1, 1, 2, 3, 5, 8, 13};
+        assertArrayEquals(expected, Main.sortArray(input));
     }
 }
